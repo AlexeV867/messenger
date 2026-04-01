33afea012eefa930a6c060ede71620a2b4fa9cc2
AlexeV867 <aleksey.vorontzoff@gmail.com>
2026-04-01 15:54:46 +0700

    firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -6,12 +6,13 @@ import { getAuth } from "firebase/auth";
 // СЮДА ВСТАВЛЯЕШЬ СВОИ ДАННЫЕ ИЗ FIREBASE (шаг 4 инструкции)
 // ============================================================
 const firebaseConfig = {
-  apiKey: "moimessenger-2ce13.firebaseapp.com",
-  authDomain: "https://moimessenger-2ce13-default-rtdb.europe-west1.firebasedatabase.app",
+  apiKey: "AIzaSyC4y4-T1DhBUNoPosopvYNrp8oZ8Yhwhm4",
+  authDomain: "moimessenger-2ce13.firebaseapp.com",
+  databaseURL: "https://moimessenger-2ce13-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "moimessenger-2ce13",
   storageBucket: "moimessenger-2ce13.firebasestorage.app",
   messagingSenderId: "249843589582",
-  appId: "1:249843589582:web:aa0ecf1b6baf84b35bcb9f",
+  appId: "1:249843589582:web:aa0ecf1b6baf84b35bcb9f"
 };
 // ============================================================
 
